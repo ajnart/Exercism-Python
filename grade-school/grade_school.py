44e041228f9639344259496d3cd89786240a239f
Thomas <thomas.camlong@epitech.eu>
2020-02-14 12:13:10 +0100

    Grade School exercice added (this one was really tricky)

---
+++ grade-school/grade_school.py
@@ -0,0 +1,15 @@
+from collections import defaultdict
+
+
+class School(object):
+    def __init__(self):
+        self.db = defaultdict(set)
+
+    def grade(self, n):
+        return sorted(list(self.db[n])) or []
+
+    def add_student(self, name, grade):
+        self.db[grade].add(name)
+
+    def roster(self):
+        return [name for k, v in sorted(self.db.items()) for name in sorted(v)]exercism download --exercise=clock --track=python
