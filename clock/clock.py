c029286739d3b9c3ad5e9c04e8883033493a8aa4
Thomas <thomas.camlong@epitech.eu>
2020-02-16 11:41:37 +0100

    Finished the clock exercice.

--- clock/clock.py
+++ clock/clock.py
@@ -1,15 +1,19 @@
 class Clock:
-    def __init__(self, hour, minute):
-        pass
+    def __init__(self, hours, minutes):
+        self.hour = (hours + (minutes // 60)) % 24
+        self.minute = minutes % 60
 
     def __repr__(self):
-        pass
+        return 'Clock({0}, {1})'.format(self.hour, self.minute)
+    
+    def __str__(self):
+        return("{0:02d}:{1:02d}".format(self.hour, self.minute))
 
     def __eq__(self, other):
-        pass
+        return self.hour == other.hour and self.minute == other.minute
 
     def __add__(self, minutes):
-        pass
+        return Clock(self.hour, self.minute + minutes) # TODO : Review this again.
 
     def __sub__(self, minutes):
-        pass
+        return Clock(self.hour, self.minute - minutes)
