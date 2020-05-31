22b0cbaa9d373ad14f4691e38a611b31c2a67815
ajnart <thomas.camlong@epitech.eu>
2020-05-31 14:23:21 +0200

    Bank_account 10 tests passing

--- bank-account/bank_account.py
+++ bank-account/bank_account.py
@@ -1,24 +1,32 @@
 class BankAccount:
     def __init__(self):
+        self.__opened = None
         self.__balance = 0
 
     def get_balance(self):
         return self.__balance
 
     def open(self):
-        self.__opened = True
+        if self.__opened == False:
+            raise ValueError("Account can't be opened twice")
+        else:
+            self.__opened = True
 
     def deposit(self, amount):
-        if self.__opened == False:
+        if self.__opened == False or amount < 0:
             raise ValueError("Account is closed")
         else:
             self.__balance += amount
 
     def withdraw(self, amount):
-        if self.__opened == False:
+        if self.__opened == False or amount > self.__balance:
             raise ValueError("Account is closed")
         else:
             self.__balance -= amount
 
     def close(self):
-        self.__opened = False
+        if self.__opened == False:
+            raise ValueError("Account not open")
+        else:
+            self.__opened = False
+            self.__balance = 0
