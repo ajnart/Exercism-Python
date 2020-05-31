a4accb503947add7345e846ad7033d64ce503122
ajnart <thomas.camlong@epitech.eu>
2020-05-31 16:19:29 +0200

    Protein translation

---
+++ protein-translation/protein_translation.py
@@ -0,0 +1,26 @@
+amino = {
+    'AUG': 'Methionine',
+    'UUU': 'Phenylalanine',
+    'UUC': 'Phenylalanine',
+    'UUA': 'Leucine',
+    'UUG': 'Leucine',
+    'UCU': 'Serine',
+    'UCC': 'Serine',
+    'UCA': 'Serine',
+    'UCG': 'Serine',
+    'UAU': 'Tyrosine',
+    'UAC': 'Tyrosine',
+    'UGU': 'Cysteine',
+    'UGC': 'Cysteine',
+    'UGG': 'Tryptophan',
+    'UAA': 'STOP',
+    'UAG': 'STOP',
+    'UGA': 'STOP',
+}
+
+def proteins(strand):
+    results = []
+    for i in range(0, len(strand), 3):
+        acid = amino.get(strand[i:i + 3])
+        results.append(acid)
+    return results
